e0fd407b1d80afe17af35a738036361a73ee87d4
epiqu1n <epiqu1n@gmail.com>
2022-06-05 23:48:17 -0400

    Added postgres example usage

---
+++ server/models/exampleModels.js
@@ -0,0 +1,48 @@
+import { Pool } from 'pg';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { zip } from '../utils/utils.js';
+import colors from 'colors';
+import fs from 'fs/promises';
+
+// Load config
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const _config = JSON.parse(await fs.readFile(path.join(__dirname, './server.config.json')));
+
+// Database schema: https://app.dbdesigner.net/designer/schema/532196
+// Initialize pool
+const pool = new Pool({
+  connectionString: _config.databaseUri
+});
+
+/**
+ * Queries the database
+ * @param {string} queryString
+ * @param {any[]} params
+ * @param {boolean} log Whether or not to log the query to the console. Default `false`
+ */
+export function query(queryString, params, log = false) {
+  if (log) console.log('Running query:\n', colors.cyan(queryString));
+  return pool.query(queryString, params);
+}
+
+/**
+ * Tagged template function which removes excess indentation from a multiline template literal
+ * @param {TemplateStringsArray} strings 
+ * @param  {...any[]} variables 
+ */
+export function sql(strings, ...variables) {
+  const str = zip(strings, variables).join('');
+  const lines = str.split(/\n/g);
+  if (lines.length > 2) {
+    const indent_0 = lines[1].search(/\S|$/m);
+    const pattern = new RegExp(`^\\s{0,${indent_0}}`, 'm');
+    for (let l = 1; l<lines.length; l++) {
+      lines[l] = lines[l].replace(pattern, '');
+    }
+  }
+  if (lines[0].trim() == '') lines.splice(0, 1);
+  if (lines[lines.length - 1].trim() == '') lines.splice(lines.length - 1, 1);
+    
+  return lines.join('\n');
+}
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,19 @@
 import { Router } from 'express';
 import apiController from '../controllers/apiController.js';
+import modelController from '../controllers/modelController.js';
 
 const apiRouter = Router();
 
 apiRouter.get('/', apiController.exampleMiddleware, function(req, res) {
   return res.json({ msg: res.locals.example });
+});
+
+apiRouter.post('/example', modelController.addExample, function(req, res) {
+  return res.json({ success: true });
+});
+
+apiRouter.get('/example', modelController.getAllExamples, function(req, res) {
+  return res.json({ examples: res.locals.examples });
 });
 
 export default apiRouter;
