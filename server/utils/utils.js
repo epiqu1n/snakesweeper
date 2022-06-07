78421864ab792c0c85ec48c757a9ea793d985afb
epiqu1n <epiqu1n@gmail.com>
2022-06-07 13:58:21 -0400

    Added custom errors and `getBodyProps`

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -1,25 +1,48 @@
 import colors from 'colors';
 
-export class ServerError extends Error {
+/** @typedef {import("express").Request} Request */
+
+export class CustomError extends Error {
+  /**
+   * @param {string} message
+   * @param {string} className
+   * @param {number} code
+   */
+  constructor(message, className = 'CustomError', code = 500) {
+    super(message);
+    this.name = className;
+    this.stack = this.cleanStacktrace(this.stack);
+    this.code = code;
+  }
+  
+    /**
+     * @private
+     * Scrubs references to node_modules from stacktrace to make it more readable
+     * @param {string} stack 
+     */
+  cleanStacktrace(stack) {
+    const newStack = stack.split(/\n/g).filter((line, i) => i == 0 || !line.match(/node_modules/));
+    return newStack.join('\n');
+  }
+}
+
+export class ServerError extends CustomError {
   /**
    * @param {string} message
    * @param {number} code
    */
   constructor(message, code = 500) {
-    super(message);
-    this.name = 'ServerError';
-    this.stack = this.cleanStacktrace(this.stack);
-    this.code = code;
+    super(message, 'ServerError', code);
   }
+}
 
+export class ClientError extends CustomError {
   /**
-   * @private
-   * Scrubs references to node_modules from stacktrace to make it more readable
-   * @param {string} stack 
+   * @param {string} message
+   * @param {number} code
    */
-  cleanStacktrace(stack) {
-    const newStack = stack.split(/\n/g).filter((line, i) => i == 0 || !line.match(/node_modules/));
-    return newStack.join('\n');
+  constructor(message, code = 400) {
+    super(message, 'ClientError', code);
   }
 }
 
@@ -50,4 +73,21 @@ export function error(message = '', ...optionalParams) {
  */
 export function warn(message = '', ...optionalParams) {
   console.warn(colors.yellow(message), ...optionalParams.map(p => colors.yellow(p)));
+}
+
+/**
+ * Extracts the desired properties from a request body and checks that they are the correct type
+ * @param {Request} req The Express request
+ * @param {{[key: string]: [type: string]}} properties An object mapping property names to their expected types
+ * @returns {{[key: string]: any}} The provided properties mapped to their values from the request body
+ * @throws {TypeError} If the request body is missing a property or the property is the incorrect type
+ */
+export function getBodyProps(req, properties) {
+  const newProps = {};
+  for (const [key, type] of Object.entries(properties)) {
+    if (typeof req.body[key] !== type || req.body[key] === '')
+      throw new ClientError(`Type "${typeof req.body[key]}" is invalid for property "${key}": "${type}"`);
+    newProps[key] = req.body[key];
+  }
+  return newProps;
 }
