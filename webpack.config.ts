c01311642b7bfa17576e131c1b8e027d6f9916cd
epiqu1n <epiqu1n@gmail.com>
2022-09-26 12:13:56 -0400

    Removed url-loader because it broke stylesheet URLs

--- webpack.config.ts
+++ webpack.config.ts
@@ -52,12 +52,12 @@ export default {
         use: 'ts-loader',
         // Use babel-loader w/ @babel/preset-typescript & react?
         exclude: /node_modules/
-      },
+      }/* ,
       {
         test: /\.png$/,
         use: 'url-loader',
         exclude: /node_modules/
-      }
+      } */
     ]
   },
   plugins: [
