2df3fb966b936e4cac85fdff5c0ef2909c27e3d7
epiqu1n <epiqu1n@gmail.com>
2022-11-03 00:49:42 -0400

    Created input modal component

---
+++ client/components/shared/InputModal.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useRef, useState } from 'react';
+import styles from './Modal.module.scss';
+
+interface InputModalProps {
+  message: string,
+  onSubmit: (value: string) => void,
+  onCancel: () => void
+}
+
+export default function InputModal({ message, onSubmit, onCancel }: InputModalProps) {
+  const dialogRef = useRef<HTMLDialogElement>(null) as React.MutableRefObject<HTMLDialogElement | null>;
+  const [ input, setInput ] = useState('');
+
+  useEffect(() => {
+    dialogRef.current?.showModal();
+  }, [dialogRef]);
+
+  return (
+    <dialog ref={dialogRef} onCancel={onCancel} className={styles.modal}>
+      <span onClick={onCancel}>X</span>
+      <pre>{message}</pre>
+      <form onSubmit={() => onSubmit(input)} method='dialog'>
+        <input value={input} onChange={(event) => setInput(event.target.value)} autoFocus />
+        <input type='submit' value='Submit' />
+      </form>
+    </dialog>
+  );
+}
