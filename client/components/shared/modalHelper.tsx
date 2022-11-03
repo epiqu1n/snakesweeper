01a11a4a09e796ddb6ea53c69976d60688464319
epiqu1n <epiqu1n@gmail.com>
2022-11-03 00:50:21 -0400

    Created async input modal spawner

---
+++ client/components/shared/modalHelper.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import InputModal from './InputModal';
+
+export default function promptModal<RT>(message: string): Promise<{ input: string | undefined, cancelled: boolean }> {
+  return new Promise((resolve) => {
+    console.debug('Starting modal');
+    const container = document.createElement('div');
+    const modalRoot = createRoot(container);
+
+    function closeModal(input: string | undefined, cancelled = false) {
+      modalRoot.unmount();
+      container.remove();
+      resolve({ input, cancelled });
+      console.debug('Closing modal');
+    }
+
+    const modal = (
+      <InputModal
+        message={message}
+        onSubmit={(input) => closeModal(input)} onCancel={() => closeModal(undefined, true)}
+      />
+    );
+
+    document.body.append(container);
+    modalRoot.render(modal);
+  });
+}
+
+// Return a Promise
+  // Create a new React root on the document body
+  // Render the modal within the root
+  // When the "yes" or "no" buttons are clicked:
+    // The promise resolves with a boolean for if the dialog was confirmed ("yes")
+    // The modal is unrendered
+    // Unmount the React app
