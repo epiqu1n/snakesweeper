f9c708dcf03b8c0ce8d4199b2f5a280957c372b0
epiqu1n <epiqu1n@gmail.com>
2022-11-03 00:56:32 -0400

    Cleaned up `promptModal`

--- client/components/shared/modalHelper.tsx
+++ client/components/shared/modalHelper.tsx
@@ -1,12 +1,14 @@
-import { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import InputModal from './InputModal';
 
 export default function promptModal<RT>(message: string): Promise<{ input: string | undefined, cancelled: boolean }> {
   return new Promise((resolve) => {
+    // Create a mini React app
     const container = document.createElement('div');
+    container.className = 'input-modal-container'
     const modalRoot = createRoot(container);
 
+    /** Removes the modal and resolves the promise */
     function closeModal(input: string | undefined, cancelled = false) {
       modalRoot.unmount();
       container.remove();
@@ -24,11 +26,3 @@ export default function promptModal<RT>(message: string): Promise<{ input: strin
     modalRoot.render(modal);
   });
 }
-
-// Return a Promise
-  // Create a new React root on the document body
-  // Render the modal within the root
-  // When the "yes" or "no" buttons are clicked:
-    // The promise resolves with a boolean for if the dialog was confirmed ("yes")
-    // The modal is unrendered
-    // Unmount the React app
