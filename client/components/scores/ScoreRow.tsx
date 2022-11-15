d8a4152b6d36602546612d2fee9cd71958d3b22d
epiqu1n <epiqu1n@gmail.com>
2022-11-14 21:03:45 -0500

    Truncate usernames

--- client/components/scores/ScoreRow.tsx
+++ client/components/scores/ScoreRow.tsx
@@ -21,14 +21,14 @@ export default function ScoreRow(props: ScoreRowProps) {
   const hours = ( rawHours === 0 ? 12 : rawHours > 12 ? rawHours % 12 : rawHours );
   const minutes = ( rawMinutes < 10 ? `0${rawMinutes}` : rawMinutes);
   const amPm = ( date.getHours() < 12 ? 'am' : 'pm' );
-  const timeZone = new Date().toLocaleTimeString('en-us',{timeZoneName:'short'}).split(' ')[2];
+  const timeZone = new Date().toLocaleTimeString('en-us', { timeZoneName: 'short' })?.split(' ')[2] || '';
   const timeStr = `${hours}:${minutes}${amPm} ${timeZone}`;
 
   return (
     <tr className={styles['row']}>
-      <td>{username}</td>
+      <td className={styles['username']}>{username}</td>
       <td align='center'>{time_seconds}s</td>
-      <td align='right' className={styles['date']}>{month} {date.getDate()}</td>
+      <td align='right' className={styles['date']}>{dateStr}</td>
       <td className={styles['date']}>at {timeStr}</td>
     </tr>
   );
