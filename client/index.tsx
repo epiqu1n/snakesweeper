e566cffb42a0b254d0d05823e7d4a6b69a52d7c1
epiqu1n <epiqu1n@gmail.com>
2022-12-07 16:15:43 -0500

    Added react query provider and devtools

--- client/index.tsx
+++ client/index.tsx
@@ -1,10 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+const queryClient = new QueryClient();
+
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(<>
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools
+        initialIsOpen={true}
+        position={'top-right'}
+        panelPosition={'right'}
+      />
+    </QueryClientProvider>
   </BrowserRouter>
 </>);
