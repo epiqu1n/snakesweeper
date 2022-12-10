dc6119b3a2d9f932fbc1ad96fc29fe40212c5ddb
epiqu1n <epiqu1n@gmail.com>
2022-12-09 19:28:05 -0500

    Added infinite scores query

--- client/api/scores/index.ts
+++ client/api/scores/index.ts
@@ -1,7 +1,8 @@
-import { createMutationHook, createQueryHook } from '../queryFactory';
+import { createInfiniteQueryHook, createMutationHook, createQueryHook } from '../queryFactory';
 import { getScores, postScore } from './methods';
 
 const SCORES = 'scores';
 
 export const useGetScores = createQueryHook(SCORES, getScores);
+export const useGetScoresInfinite = createInfiniteQueryHook(SCORES, getScores);
 export const usePostScores = createMutationHook(SCORES, postScore);
