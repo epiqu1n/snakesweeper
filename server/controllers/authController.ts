c06c37cfc48648ad006d0c5b14b2e498f5c9962b
epiqu1n <epiqu1n@gmail.com>
2022-12-12 16:26:47 -0500

    Implemented signup route

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -3,15 +3,51 @@ import Users from '../models/Users';
 import { ClientError, extractBody, PropertyMap } from '../utils/utils';
 
 enum AuthMethod {
-  VALIDATE_NEW_USER = 'validateNewUser',
-  VERIFY_USER = 'verifyUser'
+  validateNewUserInfo,
+  verifyUser
 }
 
-type AuthController = Record<AuthMethod, RequestHandler>;
+type AuthController = Record<keyof typeof AuthMethod, RequestHandler>;
 
 const authController: AuthController = {
-  validateNewUser: async (req, res, next) => {
-    next({ msg: 'This route has not been implemented yet' }); // TODO:
+  validateNewUserInfo: async (req, res, next) => {
+    const expProps = {
+      username: "string",
+      password: "string"
+    } as const;
+
+    // Validate body
+    let signupInfo: PropertyMap<typeof expProps>;
+    try {
+      signupInfo = extractBody(req, expProps);
+    } catch (err) {
+      return next({
+        msg: 'Please provide a username and password',
+        err: err
+      });
+    }
+
+    const { username, password: plainPass } = signupInfo;
+    if (plainPass.length < 8) return next({ msg: 'Your password must be longer than 8 characters', code: 400 });
+    // TODO? Check to make sure username is appropriate
+    // Word filters that aren't overly aggressive are hard though...
+
+    // Check that user doesn't exist yet
+    try {
+      const user = await Users.getUserByName(username);
+      if (user) return next({
+        msg: 'Username is already taken',
+        code: 400
+      });
+    } catch (err) {
+      return next({
+        msg: 'An error occurred validating username',
+        err: err
+      });
+    }
+
+    // A-Ok -> Create user
+    return next();
   },
   verifyUser: async (req, res, next) => {
     const expProps = {
@@ -25,7 +61,7 @@ const authController: AuthController = {
       loginInfo = extractBody(req, expProps);
     } catch (err) {
       return next({
-        msg: 'Please provide a username and password',
+        msg: 'Please enter a username and password',
         err: err
       });
     }
@@ -34,7 +70,10 @@ const authController: AuthController = {
     const { username, password: plainPass } = loginInfo;
     try {
       const passIsValid = await Users.checkPassword(username, plainPass);
-      if (!passIsValid) throw new ClientError(`Password for user ${username} does not match`, 403);
+      if (!passIsValid) return next({
+        msg: `Password for user ${username} does not match`,
+        code: 403
+      });
     } catch (err) {
       return next({
         msg: 'Invalid username or password',
