6f810e3cc9dd4d3e93712862f65bd49e63545b8f
epiqu1n <epiqu1n@gmail.com>
2022-12-16 15:50:08 -0500

    Implemented `setAuthToken` method

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,12 +1,23 @@
-import { RequestHandler } from 'webpack-dev-server';
+import { RequestHandler } from 'express';
 import Users from '../models/Users';
 import { ClientError, extractBody, PropertyMap } from '../utils/utils';
+import { sign, verify } from 'jsonwebtoken';
+import config from '../server.config.json';
+import { getLocalsUser } from '../locals/users';
 
 interface AuthController {
   /** Checks that the provided info is valid for creating a new user */
   validateNewUserInfo: RequestHandler,
   /** Checks to make sure a user exists and that the provided password is correct */
-  verifyUser: RequestHandler
+  verifyUser: RequestHandler,
+  /** Creates and sets a JWT for a user and stores it in cookies. Depends on user info present in `res.locals.user` */
+  setAuthToken: RequestHandler,
+  /**
+   * Checks the request for a valid auth token.
+   * If the token is valid, it is regenerated with a new expiration, and the user's info stored in the token is set in `res.locals.userToken`.
+   * If it is not, the token is cleared and the request is sent to the global error handler.
+   */
+  validateAuthToken: RequestHandler
 }
 
 const authController: AuthController = {
@@ -85,6 +96,34 @@ const authController: AuthController = {
 
     // All is well
     return next();
+  },
+  /** Creates and sets a JWT for a user and stores it in cookies. Depends on user info present in `res.locals.user` */
+  setAuthToken: (req, res, next) => {
+    const userInfo = getLocalsUser(res);
+    if (!userInfo) return next({
+      msg: 'An error occurred setting user credentials (ac-sat-1)',
+      err: 'User info is not present in locals'
+    });
+
+    const token = sign(userInfo, config.authSecret, { expiresIn: "2w" });
+    res.cookie('userAuth', token, {
+      maxAge: 2 * 7 * 24 * 60 * 60 * 1000, // 2 weeks,
+      httpOnly: true,
+      secure: true
+    });
+
+    // TODO: I think this is done but it should be tested
+
+    next({ msg: 'This feature is not yet available' });
+  },
+  /**
+   * Checks the request for a valid auth token.
+   * If the token is valid, it is regenerated with a new expiration, and the user's info stored in the token is set in `res.locals.userToken`.
+   * If it is not, the token is cleared and the request is sent to the global error handler.
+   */
+  validateAuthToken: (req, res, next) => {
+    // TODO:
+    next({ msg: 'This feature is not yet available' });
   }
 }
 
