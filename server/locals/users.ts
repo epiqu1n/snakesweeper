0abffe91698c588097645a4e16ec7398ee05b221
epiqu1n <epiqu1n@gmail.com>
2022-12-18 18:56:51 -0500

    Added get/set for user token

--- server/locals/users.ts
+++ server/locals/users.ts
@@ -1,5 +1,4 @@
 import { Response } from 'express';
-import { AuthToken } from '../controllers/authController';
 import { UserInfo } from '../models/Users';
 
 export function getLocalsUser(res: Response) {
@@ -8,15 +7,6 @@ export function getLocalsUser(res: Response) {
 
 export function setLocalsUser(res: Response, userInfo: UserInfo) {
   res.locals.user = userInfo;
-  return;
-}
-
-export function getLocalsUserToken(res: Response) {
-  return res.locals.userToken as Maybe<AuthToken>;
-}
-
-export function setLocalsUserToken(res: Response, userToken: AuthToken) {
-  res.locals.userToken = userToken;
   return;
 }
 
