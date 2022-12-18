ba1d80027c2dfcaf7bcad19c368667e14e1b5993
epiqu1n <epiqu1n@gmail.com>
2022-12-18 18:57:13 -0500

    Implemented logout route

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,16 +1,19 @@
 import { Router } from 'express';
 import authController from '../controllers/authController';
 import userController from '../controllers/userController';
+import { getLocalsUser } from '../locals/users';
 
 const authRouter = Router();
 
 authRouter.post('/signup', authController.validateNewUserInfo, userController.addUser, (req, res) => {
-  res.json({ success: true });
+  res.json({});
 });
 
-authRouter.post('/login', authController.verifyUser, authController.setAuthToken, (req, res) => {
+authRouter.post('/login', authController.verifyLoginAttempt, authController.setAuthToken, (req, res) => {
   // TODO: Add updateLastLogin once implemented
-  res.json({ success: true });
+  res.json({ data: getLocalsUser(res) });
 });
+
+authRouter.post('/logout', authController.clearAuthToken, (req, res) => res.json({}));
 
 export default authRouter;
