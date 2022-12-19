668a5f85d9a9b6661411e89ca65b7baf670517f9
epiqu1n <epiqu1n@gmail.com>
2022-12-18 21:39:54 -0500

    Moved user signup route to be proper REST route

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,13 +1,8 @@
 import { Router } from 'express';
 import authController from '../controllers/authController';
-import userController from '../controllers/userController';
 import { getLocalsUser } from '../locals/users';
 
 const authRouter = Router();
-
-authRouter.post('/signup', authController.validateNewUserInfo, userController.addUser, (req, res) => {
-  res.json({});
-});
 
 authRouter.post('/login', authController.verifyLoginAttempt, authController.setAuthToken, (req, res) => {
   // TODO: Add updateLastLogin once implemented
